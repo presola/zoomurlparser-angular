73a48e3c11f0128129c87c394dbe4ac600eac8d0
presola <53655433+presola@users.noreply.github.com>
2021-02-17 03:13:50 -0400

    add nodejs

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+app.use(express.static(__dirname + '/dist/ZoomURLParserAngular'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/ZoomURLParserAngular/index.html'));
+});
+console.log(`Listening on http://0.0.0.0:${process.env.PORT || 8080}`)
+app.listen(process.env.PORT || 8080);
